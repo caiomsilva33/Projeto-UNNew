e4a220af7416debfca7d70aad557aee222e5022c
Kaito Silva <caiomsilva1704@gmail.com>
2023-05-29 15:02:02 -0400

    caiomsilva33: Banco de Dados - Django

---
+++ Projeto_UNNEW2/ProjetosUNN/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProjetosunnConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ProjetosUNN'
---
+++ Projeto_UNNEW2/ProjetosUNN/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def projetos(request):
+    return render(request, 'telaprincipal.html'),
